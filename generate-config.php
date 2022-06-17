38640a45fddbd1e30bb6303bd0678b84a764df40
Raimondas Rimkevičius <github@mekdrop.name>
2022-06-18 01:51:23 +0300

    Create generate-config.php

---
+++ generate-config.php
@@ -0,0 +1,25 @@
+<?php
+
+$outputFile = $argv[1];
+$classesListFile = $argv[2];
+$rootNamespace = $argv[3];
+$outputDocsPath = $argv[4];
+
+shell_exec('mkdir -p "' . $outputDocsPath . '"');
+
+$outputDocsPath = realpath($outputDocsPath);
+
+file_put_contents(
+  $outputFile,
+  '<?php' . PHP_EOL .
+  PHP_EOL .
+  'return (object)' . var_export(
+    [
+      'rootNamespace' => $rootNamespace,
+      'destDirectory' => $outputDocsPath,
+      'format' => 'github',
+      'classes' => file($outputFile),
+    ],  
+    true
+   ) . ';' ;
+);
